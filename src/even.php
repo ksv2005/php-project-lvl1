a203198fc997b13d20d99eab201f6e6a9955cade
sergey <sergey@designmix.ru>
2019-10-06 12:37:02 +0300

    Add Brain Even Game

---
+++ src/even.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BrainGames\Even;
+
+use function cli\line;
+use function cli\prompt;
+
+
+define('COUNT_GAMES', 3);
+
+function run()
+{
+    line('Welcome to Brain Games!');
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    line('');
+
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('');
+
+    $count = 0;
+    while($count < COUNT_GAMES) {
+        if(game($name)) {
+            $count++;
+        } else {
+            return;
+        }
+    }
+    line("Congratulations, %s!", $name);
+}
+
+function game($name)
+{
+    $number = rand(1, 99);
+    line("Question: %s", $number);
+    $answer = prompt('Your answer');
+    $true_answer = ($number % 2 == 0) ? 'yes' : 'no';
+    if($answer == $true_answer) {
+        line('Correct!');
+        return true;
+    } else {
+        line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $true_answer);
+        line("Let's try again, %s!", $name);
+        return false;
+    }
+}
