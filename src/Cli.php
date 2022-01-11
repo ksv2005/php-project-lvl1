1a71d54fccada3ade5a4bd8a287d1fa72c763190
= <Woolfer0097@yandex.ru>
2022-01-11 16:58:06 +0300

    4 step

--- src/Cli.php
+++ src/Cli.php
@@ -1,1 +1,11 @@
 <?php
+
+use function cli\line;
+use function cli\prompt;
+
+function greetings()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
