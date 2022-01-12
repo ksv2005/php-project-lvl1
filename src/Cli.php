4c206c2d5a02ec14829854a90e40cd560fcb77fd
= <Woolfer0097@yandex.ru>
2022-01-12 22:53:45 +0300

    final commit #3

--- src/Cli.php
+++ src/Cli.php
@@ -9,7 +9,7 @@ function run()
 {
     function greetings()
     {
-        line('Welcome to the Brain Game!');
+        line('/Welcome to the Brain Games!/');
         $name = prompt('May I have your name?');
         line("Hello, %s!", $name);
     }
