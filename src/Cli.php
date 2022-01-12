e4fbd74e4677fa85e4d90f413603da36e4980866
= <Woolfer0097@yandex.ru>
2022-01-12 22:51:18 +0300

    final commit #3

--- src/Cli.php
+++ src/Cli.php
@@ -1,11 +1,17 @@
 <?php
 
+namespace BrainGames\Cli;
+
 use function cli\line;
 use function cli\prompt;
 
-function greetings()
+function run()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    function greetings()
+    {
+        line('Welcome to the Brain Game!');
+        $name = prompt('May I have your name?');
+        line("Hello, %s!", $name);
+    }
+    greetings();
 }
